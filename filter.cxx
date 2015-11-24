9716337ec4394ba900087f84924ca3afb1804796
Konstantin Beyer <kobey100@pc06.pool.tp1.uni-duesseldorf.de>
2015-11-24 16:55:58 +0100

    annotations

--- filter.cxx
+++ filter.cxx
@@ -11,25 +11,25 @@ int main(){
 const int N=237, iterations=15;
 const string filenameInitial="noisy.txt";
 const string filenameEnd="filtered.txt";
-double* const data= read(N,filenameInitial);
-for (int i=0;i<iterations;i++) filter(data,N);
+double* const data= read(N,filenameInitial); //pointer to array with data
+for (int i=0;i<iterations;i++) filter(data,N); //multiple filtering
 write(data,N,filenameEnd);
-delete[] data;
+delete[] data;  // delete the dynamic allocated memory before finishing the program
 return 0;
 }
 
 double* const read(const int N, const string filename){
 ifstream in(filename.c_str());
-double* const data = new double[N];
-	for(int i=0;i<N;i++) in >> data[i];
+double* const data = new double[N];  // dynamically allocate memory for the data
+	for(int i=0;i<N;i++) in >> data[i];  //read the data
 in.close();
-return data;
+return data;  // return the pointer to the array with the data
 }
 
 void filter(double* const data, const int N){
 const double f0=0.1;
 double a=f0,b;
-	for(int i=0;i<N-1;i++){
+	for(int i=0;i<N-1;i++){  //calculate the local average
 		b=data[i];
 		data[i]=(a+b+data[i+1])/double(3);
 		a=b;
